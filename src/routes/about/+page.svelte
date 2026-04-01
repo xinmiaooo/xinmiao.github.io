9e56681005e19ba049d51ea55a80f92f8ea6f504
Qu Xinmiao <83005951+xinmiaooo@users.noreply.github.com>
2026-04-01 19:29:41 +0900

    about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,32 @@
-<div class="container mx-auto px-4 py-8">
-	<h1 class="text-3xl font-bold">About</h1>
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.png';
+</script>
+
+<div class="container mx-auto flex flex-col items-center gap-6 px-4 py-16">
+	<img src={favicon} alt="Profile" class="h-40 w-40 rounded-full" />
+	<div>
+		<h1 class="mb-4 text-3xl font-bold">Xinmiao Qu</h1>
+		<div class="flex flex-col justify-center gap-3 sm:flex-row">
+			<a
+				href="https://github.com/xinmiaooo"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="inline-flex items-center gap-2 text-lg text-muted-foreground transition-colors hover:text-foreground"
+			>
+				GitHub
+			</a>
+			<span class="inline-flex items-center gap-2 text-lg text-muted-foreground transition-colors">
+				/
+			</span>
+
+			<a
+				href="https://qiita.com/xinmiao1995"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="inline-flex items-center gap-2 text-lg text-muted-foreground transition-colors hover:text-foreground"
+			>
+				Qiita
+			</a>
+		</div>
+	</div>
 </div>
